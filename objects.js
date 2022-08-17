ec8319431da0065f2cf3ea2454180499af4ad4d4
Pavan raju <pavankumarraju7@gmail.com>
2022-08-17 15:28:51 +0530

    object js

---
+++ objects.js
@@ -0,0 +1,5 @@
+emp={id:102,name:"Shyam Kumar",salary:40000}  
+console.log(emp.id+" "+emp.name+" "+emp.salary); 
+
+// in above case emp is object
+//id , name is properited and numbers are values.
